fcec99e563778429cbfccc720e143d66fab321b0
Autumn (Arch) <arch1010@pm.me>
2025-01-23 20:30:15 -0600

    (Feat): No cache

--- web_files/index.js
+++ web_files/index.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/title/')
+    fetch('./title/')
         .then(response => response.json())
         .then(data => {
             document.getElementById('jsUpdate').textContent = data.title;
             document.getElementById('titleChange').textContent = "Welcome to " + data.title;
+            document.getElementById('setImage').src = "./output/weather_forecast.png?nocache=" + new Date().getTime();
         })
         .catch(error => console.error('Error fetching title:', error));
 });
